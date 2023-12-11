03503e7991df4c9841dd76370adaba37853e7210
djhelam <jhelam.deshpande@students.iiserpune.ac.in>
2023-12-11 11:25:09 +0100

    added dispersal and dispersal evolution

--- metapopulation_ibm.cpp
+++ metapopulation_ibm.cpp
@@ -33,11 +33,13 @@ class TInd  //this defines a class of individuals
   TInd();  
   double lambda;
   double alpha;
+  double dispersal_probability;
 };
 TInd::TInd()
 {
   lambda=0.0;
   alpha=0.0;
+  dispersal_probability=0.0;
 }
 
 class TPatch
@@ -46,22 +48,28 @@ class TPatch
   TPatch();
   vector<TInd> females;
   vector<TInd> newfemales;
+  double measured_dispersal;
 };
 TPatch::TPatch()
 {
   females.clear();
   newfemales.clear();
+  measured_dispersal=0.0;
 }
 
-TPatch world;
+const int NUMBER_OF_PATCHES=1;
 
+TPatch world[NUMBER_OF_PATCHES];
+
+int REPLICATES=2;
 int N0=10;
 double LAMBDA=2.0;
 double ALPHA=0.01;
 int TMAX=100;
-
-
-
+double DISPERSAL_PROBABILITY=0.5;
+double DISPERSAL_MORTALITY=0;
+double MUTATION_PROBABILITY=0.0;
+double MUTATION_EFFECT_SD=0.1;
 
 
 
@@ -109,43 +117,117 @@ const int RS = 100;                 // random seed
 
 void initialise_world()
 {
-  world.females.clear();
-  world.newfemales.clear();
-  for(int n=0;n<N0;n++)
+  for(int x=0;x<NUMBER_OF_PATCHES;x++)
   {
-    TInd newind;
-    newind.lambda=LAMBDA;
-    newind.alpha=ALPHA;
-    world.females.push_back(newind);
+    world[x].females.clear();
+    world[x].newfemales.clear();
+    world[x].measured_dispersal=0.0;
+    for(int n=0;n<N0;n++)
+    {
+      TInd newind;
+      newind.lambda=LAMBDA;
+      newind.alpha=ALPHA;
+      newind.dispersal_probability=DISPERSAL_PROBABILITY;
+      world[x].females.push_back(newind);
+    }
   }
 
 }
 
+int find_patch(int x)
+{
+  int new_patch;
+  if(NUMBER_OF_PATCHES>1)
+  {
+    do
+    {
+      new_patch=floor(ran()*NUMBER_OF_PATCHES);
+    }
+    while(new_patch==x);
+  }
+  else new_patch=x;
+  return new_patch;
+}
+
+void disperse()
+{
+  for(int x=0;x<NUMBER_OF_PATCHES;x++)
+  {
+    world[x].newfemales.clear();
+    int population_size=world[x].females.size();
+    int count_dispersers=0;
+    for(int f=0; f<world[x].females.size();f++)
+    {
+      if(ran()<world[x].females.at(f).dispersal_probability)
+      {
+        count_dispersers++;
+        int new_patch;
+        new_patch=find_patch(x);
+        if(ran()<1-DISPERSAL_MORTALITY)
+        {
+          world[new_patch].newfemales.push_back(world[x].females.at(f));
+        }
+        world[x].females.erase(world[x].females.begin()+f);
+        f--;
+      }
+    }
+    if(world[x].females.size()>0)
+      world[x].measured_dispersal=double(count_dispersers)/double(population_size);
+    else
+      world[x].measured_dispersal=0.0;
+    
+  }
+  for(int x=0;x<NUMBER_OF_PATCHES;x++)
+  {
+    for(int nf=0;nf<world[x].newfemales.size();nf++)
+    {
+      world[x].females.push_back(world[x].newfemales.at(nf));
+    }
+    world[x].newfemales.clear();
+  }
+}
+
+double mutation(double dispersal)
+{
+  if(ran()<MUTATION_PROBABILITY)
+  {
+    dispersal=dispersal+gauss(MUTATION_EFFECT_SD);
+  }
+  return dispersal;
+}
+
 void reproduce()
 {
-  double alpha_sum=0.0;
-  for(int f=0;f<world.females.size();f++)
+  for(int x=0;x<NUMBER_OF_PATCHES;x++)
   {
-    alpha_sum=alpha_sum+world.females.at(f).alpha;
-  }
-  for(int f=0;f<world.females.size();f++)
-  {
-    double number_of_offspring=poisson(world.females.at(f).lambda/(1+alpha_sum));
-    for(int no=0;no<number_of_offspring;no++)
+    double alpha_sum=0.0;
+    for(int f=0;f<world[x].females.size();f++)
     {
-      TInd newind;
-      newind.lambda=world.females.at(f).lambda;
-      newind.alpha=world.females.at(f).alpha; 
-      world.newfemales.push_back(newind);   
+      alpha_sum=alpha_sum+world[x].females.at(f).alpha;
+    }
+    for(int f=0;f<world[x].females.size();f++)
+    {
+      double number_of_offspring=poisson(world[x].females.at(f).lambda/(1+alpha_sum));
+      for(int no=0;no<number_of_offspring;no++)
+      {
+        TInd newind;
+        newind.lambda=world[x].females.at(f).lambda;
+        newind.alpha=world[x].females.at(f).alpha; 
+        newind.dispersal_probability=mutation(world[x].females.at(f).dispersal_probability);
+        world[x].newfemales.push_back(newind);   
+      }
     }
   }
 }
 
 void death()
 {
-  world.females.clear();
-  world.females=world.newfemales;
-  world.newfemales.clear();
+  for(int x=0;x<NUMBER_OF_PATCHES;x++)
+  {
+    world[x].females.clear();
+    world[x].females=world[x].newfemales;
+    world[x].newfemales.clear();
+  }
 }
 
 //________________________________________________________________________________________
@@ -156,14 +238,25 @@ output of generated data, initialisation and running the life cycle of our model
 organism.*/
 int main()
 {
+  ofstream op;
+  op.open("output.txt");
+  op<<"r t x N measured_dispersal"<<endl;
   specify_rng(RS);
-  initialise_world(); //here we initialise environement
-  for(int t=0;t<TMAX;t++) //we go through the
+  for(int r=0;r<REPLICATES;r++)
   {
-    cout<<t<<" "<<world.females.size()<<endl;
-    reproduce();
-    death();
+    initialise_world(); //here we initialise environement
+    for(int t=0;t<TMAX;t++) //we go through the
+    {
+      for(int x=0;x<NUMBER_OF_PATCHES;x++)
+      {
+        op<<r<<" "<<t<<" "<<x<<" "<<world[x].females.size()<<" "<<world[x].measured_dispersal<<endl;
+      }
+     disperse();
+     reproduce();
+     death();
   }
+  }
+  op.close();
 
 
   return 0;
